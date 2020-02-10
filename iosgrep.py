3f4106fa4c292be0fac5926868b1763869786b93
AStar <sda06407@gmail.com>
2020-02-10 17:43:25 +0800

    update

--- iosgrep.py
+++ iosgrep.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import sys, os, hashlib, re
 
 def Search_in_Data(keywordInData, pgname):
         print("# grep -ril -e '%s'  %s :" %(keywordInData, pgname))
-        recvAll = os.popen("grep -ril -e '%s' %s  ; echo echo:$?" %(keywordInData, pgname)).read()
+        recvAll = os.popen(" grep -ril -e '%s' %s  ; echo echo:$?'" %(keywordInData, pgname)).read()
         recvCode = re.search('(?<=echo:)\d+', recvAll)
         print("Find" if recvCode.group(0)=='0' else "Not Found")
         print("------")
@@ -26,8 +27,11 @@ if __name__=="__main__":
         Search_in_Data(hashlib.sha1(keyword[:-1]).hexdigest(), packagename)
         Search_in_SystemCatch(hashlib.sha1(keyword[:-1]).hexdigest())
         print("---use utf-8 to search---\n")
-        Search_in_Data(keyword[:-1].decode("utf-8").encode("utf-8"), packagename)
-        Search_in_SystemCatch(keyword[:-1].decode("utf-8").encode("utf-8"))
+        Search_in_Data(repr(keyword[:-1]).replace("'",""), packagename)
+        Search_in_SystemCatch(repr(keyword[:-1]).replace("'",""))
         print("---use binary to search---\n")
-        Search_in_Data(''.join(map(bin,bytearray(keyword[:-1]))), packagename)
-        Search_in_SystemCatch(''.join(map(bin,bytearray(keyword[:-1]))))
+        Search_in_Data((' '.join(format(ord(x), 'b')for x in keyword[:-1]).replace(' ','\s')), packagename)
+        Search_in_SystemCatch((' '.join(format(ord(x), 'b')for x in keyword[:-1]).replace(' ','\s')))
+        print("---use unicode to search---")
+        Search_in_Data(repr(keyword[:-1].decode("utf-8")).replace("u", "", 1).replace("'", ""), packagename)
+        Search_in_SystemCatch(repr(keyword[:-1].decode("utf-8")).replace("u", "", 1).replace("'", ""))
